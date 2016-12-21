4d8d8a59ca74366e6f634e549630586f92857292
Pedro Luiz Cumino <pedro.cumino@gmail.com>
2016-12-21 18:21:11 -0300

    adding ModJson class

--- Linux/changeOSM/osmHandler.cpp
+++ Linux/changeOSM/osmHandler.cpp
@@ -1,26 +1,121 @@
+#include <cstdint>  // for std::uint64_t
 #include <cstdlib>  // for std::exit
-#include <iostream> // for std::cerr
+#include <iostream> // for std::cout, std::cerr
 
 // Allow any format of input files (XML, PBF, ...)
 #include <osmium/io/any_input.hpp>
 
+// We want to use the handler interface
+#include <osmium/handler.hpp>
+
+// Utility class gives us access to memory usage information
+#include <osmium/util/memory.hpp>
+
+// For osmium::apply()
+#include <osmium/visitor.hpp>
+
+#include "ModJson.h"
+
+
+using namespace std;
+
+class NamesHandler : public osmium::handler::Handler {
+
+	void output_pubs(const osmium::OSMObject& object) {
+		const osmium::TagList& tags = object.tags();
+		if (tags.has_tag("pollution", "low")) {
+
+			// Print name of the pub if it is set.
+			const char* name = tags["name"];
+			if (name) {
+				std::cout << name << "\n";
+			} else {
+				std::cout << "pub with unknown name\n";
+			}
+
+			// Iterate over all tags finding those which start with "addr:"
+			// and print them.
+			for (const osmium::Tag& tag : tags) {
+				if (!std::strncmp(tag.key(), "addr:", 5)) {
+					std::cout << "  " << tag.key() << ": " << tag.value() << "\n";
+				}
+			}
+		}
+	}
+
+public:
+
+	// Nodes can be tagged amenity=pub.
+	void node(const osmium::Node& node) {
+		output_pubs(node);
+	}
+	
+	// Ways can be tagged amenity=pub, too (typically buildings).
+	void way(const osmium::Way& way) {
+		output_pubs(way);
+	}
+
+}; // class NamesHandler
+
+struct CountHandler : public osmium::handler::Handler {
+
+	std::uint64_t nodes     = 0;
+	std::uint64_t ways      = 0;
+	std::uint64_t relations = 0;
+
+	// This callback is called by osmium::apply for each node in the data.
+	void node(const osmium::Node&) noexcept {
+		++nodes;
+	}
+
+	// This callback is called by osmium::apply for each way in the data.
+	void way(const osmium::Way&) noexcept {
+		++ways;
+	}
+
+	// This callback is called by osmium::apply for each relation in the data.
+	void relation(const osmium::Relation&) noexcept {
+		++relations;
+	}
+
+}; // struct CountHandler
+
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " OSMFILE\n";
-        std::exit(1);
-    }
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " OSMFILE\n";
+		std::exit(1);
+	}
 
-    // The Reader is initialized here with an osmium::io::File, but could
-    // also be directly initialized with a file name.
-    osmium::io::File input_file{argv[1]};
-    osmium::io::Reader reader{input_file};
+	ModJson jsontool;
+	jsontool.setFile("sensorData.json");
+	// jsontool.printList();
+	// vector< vector<string> > vec = jsontool.getVecList();
+	//jsontool.printList(vec);
 
-    // OSM data comes in buffers, read until there are no more.
-    while (osmium::memory::Buffer buffer = reader.read()) {
-    }
 
-    // You do not have to close the Reader explicitly, but because the
-    // destructor can't throw, you will not see any errors otherwise.
-    reader.close();
+	// The Reader is initialized here with an osmium::io::File, but could
+	// also be directly initialized with a file name.
+	osmium::io::File input_file{argv[1]};
+	osmium::io::Reader reader{input_file};
+
+	CountHandler handler;
+	osmium::apply(reader, handler);
+
+	// You do not have to close the Reader explicitly, but because the
+	// destructor can't throw, you will not see any errors otherwise.
+	reader.close();
+
+	cout << "Nodes\t=\t" << handler.nodes << endl;
+	cout << "Ways\t=\t" << handler.ways << endl;
+	cout << "Rel\t=\t" << handler.relations << endl;
+
+	osmium::MemoryUsage memory;
+
+	cout << "Mem used = " << memory.peak() << "MBytes" << endl;
+
+	NamesHandler names_handler;
+	osmium::io::Reader reader2{argv[1], osmium::osm_entity_bits::node | osmium::osm_entity_bits::way};
+	osmium::apply(reader2, names_handler);
+
 }
 
